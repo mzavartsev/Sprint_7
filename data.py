5fefdcd910b18a216be32744ccbd06d6c20ae1b8
Maxim <zavartsevm@gmail.com>
2024-12-08 12:21:49 +0300

    add randomizer and data

---
+++ data.py
@@ -0,0 +1,14 @@
+BASE_URL = "https://qa-scooter.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+LOGIN_COURIER = "/api/v1/courier/login"
+DELETE_COURIER = "/api/v1/courier/:id"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER = "/api/v1/orders/track?t="
+GET_LIST_OF_ORDERS = "/api/v1/orders"
+DELETE_ORDER = "/api/v1/orders/finish/:id"
+
+creds_of_an_already_created_courier = {
+    "login": "ninja",
+    "password": "1234",
+    "firstName": "saske"
+}
