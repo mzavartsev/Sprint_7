81085865320f7ef3c00203bc6e270a874be358b1
Maxim <zavartsevm@gmail.com>
2024-12-08 12:27:26 +0300

    test get order

---
+++ tests/test_get_order_using_his_number.py
@@ -0,0 +1,25 @@
+from methods.order_methods import *
+import allure
+
+
+class TestGetOrder:
+
+    @allure.title("Возвращается объект")
+    @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
+    def test_successful_return_object(self):
+        new_order = OrdersMethods()
+        new_order_info = new_order.post_create_order()
+        order_info = new_order.get_get_order_info(new_order_info[2]["track"])
+        assert "order" in order_info[2]
+
+    @allure.title("Отображается сообщение об ошибке после запроса без id")
+    @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
+    def test_error_message_after_request_without_track_id(self):
+        new_order = requests.get(f"https://qa-scooter.praktikum-services.ru/api/v1/orders/track?t=")
+        assert 400 == new_order.status_code
+
+    @allure.title("Отображается сообщение об ошибке после запроса c некорректным id")
+    @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
+    def test_error_message_after_request_with_not_correct_track_id(self):
+        new_order = requests.get(f"https://qa-scooter.praktikum-services.ru/api/v1/orders/track?t=9999999")
+        assert 404 == new_order.status_code
