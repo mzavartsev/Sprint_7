766072670abe30e4168c7b1d847b8e8ed62c1c34
Maxim <zavartsevm@gmail.com>
2024-12-13 12:14:38 +0300

    modify tests

--- tests/test_delete_courier.py
+++ tests/test_delete_courier.py
@@ -7,7 +7,7 @@ class TestDeleteCourier:
     @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
     def test_error_message_after_unsuccessful_request(self):
         delete_courier = requests.delete(f"{BASE_URL}/api/v1/courier/9999999")
-        assert delete_courier.status_code == 404
+        assert "Курьера с таким id нет" in delete_courier.json()["message"]
 
     @allure.title("Отображается корректное сообщение успеха")
     @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
@@ -21,5 +21,5 @@ class TestDeleteCourier:
     @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
     def test_return_error_message_after_delete_without_required_field(self):
         delete_courier = requests.delete(f"{BASE_URL}/api/v1/courier/")
-        assert delete_courier.status_code == 404
+        assert "Not Found" in delete_courier.json()["message"]
 
--- tests/test_get_order_using_his_number.py
+++ tests/test_get_order_using_his_number.py
@@ -16,10 +16,10 @@ class TestGetOrder:
     @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
     def test_error_message_after_request_without_track_id(self):
         new_order = requests.get(f"https://qa-scooter.praktikum-services.ru/api/v1/orders/track?t=")
-        assert new_order.status_code == 400
+        assert "Недостаточно данных для поиска" in new_order.json()["message"]
 
     @allure.title("Отображается сообщение об ошибке после запроса c некорректным id")
     @allure.link(BASE_URL, name="https://qa-scooter.praktikum-services.ru")
     def test_error_message_after_request_with_not_correct_track_id(self):
         new_order = requests.get(f"https://qa-scooter.praktikum-services.ru/api/v1/orders/track?t=9999999")
-        assert new_order.status_code == 404
+        assert "Заказ не найден" in new_order.json()["message"]
